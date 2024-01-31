86d09c8b3ecc3f6801e42b8c187e747a8d08f188
Axel Joly <azn9.pro@gmail.com>
2024-01-30 21:53:38 -0500

    Initialize repo

---
+++ Plugin.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BetterQuotaDays.Patches;
+using HarmonyLib;
+
+namespace BetterQuotaDays
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LoggerInstance;
+        
+        private void Awake()
+        {
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll(typeof(TimeOfDayPatch));
+            
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            LoggerInstance = Logger;
+        }
+    }
+}
+
+namespace BetterQuotaDays.Patches
+{
+    [HarmonyPatch(typeof(TimeOfDay))]
+    internal class TimeOfDayPatch
+    {
+        
+        [HarmonyPatch("SetNewProfitQuota")]
+        [HarmonyPostfix]
+        private static void Postfix(TimeOfDay __instance)
+        {
+            var days = 4 + __instance.profitQuota / 1000;
+            __instance.timeUntilDeadline = __instance.totalTime * days;
+            __instance.quotaVariables.deadlineDaysAmount = days;
+            
+            Plugin.LoggerInstance.LogInfo($"New deadline: {days} days");
+        }
+        
+        [HarmonyPatch("SyncNewProfitQuotaClientRpc")]
+        [HarmonyPostfix]
+        private static void Postfix2(TimeOfDay __instance)
+        {
+            var days = 4 + __instance.profitQuota / 1000;
+            __instance.timeUntilDeadline = __instance.totalTime * days;
+            __instance.quotaVariables.deadlineDaysAmount = days;
+            
+            Plugin.LoggerInstance.LogInfo($"New deadline: {days} days");
+        }
+        
+    }
+}
